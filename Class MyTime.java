d0b2ae5b2c581b89d4c4f3ff0c492da7b929fa65
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 17:51:24 +0300

    .

---
+++ Class MyTime.java
@@ -0,0 +1,114 @@
+public class MyTime {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public MyTime() {
+        this(0, 0, 0);
+    }
+
+    public MyTime(int hour, int minute, int second) {
+        setTime(hour, minute, second);
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        setHour(hour);
+        setMinute(minute);
+        setSecond(second);
+    }
+
+    public void setHour(int hour) {
+        if (hour >= 0 && hour < 24) {
+            this.hour = hour;
+        } else {
+            this.hour = 0;
+        }
+    }
+
+    public void setMinute(int minute) {
+        if (minute >= 0 && minute < 60) {
+            this.minute = minute;
+        } else {
+            this.minute = 0;
+        }
+    }
+
+    public void setSecond(int second) {
+        if (second >= 0 && second < 60) {
+            this.second = second;
+        } else {
+            this.second = 0;
+        }
+    }
+
+    
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    // Time manipulation methods
+    public MyTime nextSecond() {
+        second++;
+        if (second >= 60) {
+            second = 0;
+            nextMinute();
+        }
+        return this;
+    }
+
+    public MyTime nextMinute() {
+        minute++;
+        if (minute >= 60) {
+            minute = 0;
+            nextHour();
+        }
+        return this;
+    }
+
+    public MyTime nextHour() {
+        hour++;
+        if (hour >= 24) {
+            hour = 0;
+        }
+        return this;
+    }
+
+    public MyTime previousSecond() {
+        second--;
+        if (second < 0) {
+            second = 59;
+            previousMinute();
+        }
+        return this;
+    }
+
+    public MyTime previousMinute() {
+        minute--;
+        if (minute < 0) {
+            minute = 59;
+            previousHour();
+        }
+        return this;
+    }
+
+    public MyTime previousHour() {
+        hour--;
+        if (hour < 0) {
+            hour = 23;
+        }
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+}
