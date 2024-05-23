a4c03e73ff1f18a8e6db5d65d344a0a50c2553d6
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-05-24 00:20:15 +0300

    .

---
+++ HW Product.java
@@ -0,0 +1,35 @@
+public class Product {
+    private String code;
+    private String description;
+    private double price;
+
+    public Product(String code, String description, double price) {
+        this.code = code;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getFormattedPrice() {
+        return String.format("%.2f", price);
+    }
+}
